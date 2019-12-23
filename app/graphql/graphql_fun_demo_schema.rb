c16b07740063daff7be2e30710f71af8517f4de0
Tanbir Hasan <tanbir2025@gmail.com>
2019-12-23 21:53:59 +0600

    add graphql

---
+++ app/graphql/graphql_fun_demo_schema.rb
@@ -0,0 +1,4 @@
+class GraphqlFunDemoSchema < GraphQL::Schema
+  mutation(Types::MutationType)
+  query(Types::QueryType)
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+100.times do
+  user = User.create(name: Faker::Name.name, email: Faker::Internet.email)
+  100.times do
+   Post.create(title: Faker::Lorem.sentence(word_count: 3), body: Faker::Lorem.paragraph(sentence_count: 3), user_id: user.id)
+  end  
+end  
